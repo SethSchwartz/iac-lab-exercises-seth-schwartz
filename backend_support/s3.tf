6482c20520fc1541d7e0509ce929fc29c2b33c5e
SethSchwartz <seth.schwartz@thoughtworks.com>
2025-04-09 14:46:54 -0400

    Session 3 complete

---
+++ backend_support/s3.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "backend-s3-ss" {
+  bucket = "${var.prefix}-bucket"
+  force_destroy = true
+
+  tags = {
+    Name        = "${var.prefix}-tfstate"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "backend-s3-versioning" {
+  bucket = aws_s3_bucket.backend-s3-ss.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend_s3_encryption_config" {
+  bucket = aws_s3_bucket.backend-s3-ss.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "backend_s3_access_rules" {
+  bucket = aws_s3_bucket.backend-s3-ss.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
