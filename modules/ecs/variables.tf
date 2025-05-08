f21a2e0405e274d13903e6bcf70a125364401b12
SethSchwartz <seth.schwartz@thoughtworks.com>
2025-05-08 13:00:44 -0400

    Add variables to module directory and add output.tf

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -17,3 +17,20 @@ variable "region" {
   type        = string
   description = "region"
 }
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "List of private subnet IDs"
+}
+variable "alb_target_group_arn" {
+  type        = string
+  description = "ALB Target Group ARN"
+}
+variable "alb_security_group_id" {
+  type        = string
+  description = "ALB Security Group ID"
+}
