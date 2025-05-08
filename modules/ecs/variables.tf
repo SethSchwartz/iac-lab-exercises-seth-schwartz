332626768398162e2ffa18770d683054986ed49c
SethSchwartz <seth.schwartz@thoughtworks.com>
2025-05-08 13:14:32 -0400

    Remove variables that are defined in outputs

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,13 +1,3 @@
-output "ecr_url" {
-  description = "The Elastic Container Registry (ECR) URL."
-  value       = aws_ecr_repository.api.repository_url
-}
-
-output "ecs_security_group_id" {
-  description = "ECS Security group Id"
-  value       = aws_security_group.ecs.id
-}
-
 variable "prefix" {
   type        = string
   description = "prefix"
